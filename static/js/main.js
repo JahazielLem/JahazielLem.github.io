751f73022c1f4c46ff0f7e6425547931f4399c6c
Kevin Jahaziel Leon Morales <cresofmx@gmail.com>
2021-02-02 21:10:06 -0600

    Arreglos

---
+++ static/js/main.js
@@ -0,0 +1,11 @@
+mediumZoom('.zoom',{
+    margin: 50
+})
+mediumZoom('.zoom-dark', {
+    background: '#000'
+})
+mediumZoom('.zoom-tmp', {
+    template: '#zoom-template',
+    container: '#zoom-container',
+    background: '#000'
+})
